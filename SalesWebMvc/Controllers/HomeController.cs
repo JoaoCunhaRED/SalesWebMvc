0c1c7bd044e7196786403a4378cbdd554722fda4
JoaoCunhaRED <joao.cunha@reditpro.com>
2021-12-02 12:08:19 +0000

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,10 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC App from C# Course.";
+            ViewData["email"] = "joao@gmail.com";
+            ViewData["Developer"] = "João Cunha";
+
 
             return View();
         }
